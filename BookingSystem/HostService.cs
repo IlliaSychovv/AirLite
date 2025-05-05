e008a95826aa78a72fa423c8697ce9c543e029f0
Illia <illiasychov@gmail.com>
2025-05-05 21:56:13 +0300

    Fixed

--- BookingSystem/HostService.cs
+++ BookingSystem/HostService.cs
@@ -20,8 +20,7 @@ public class HostService
         var newHost = new Host
         {
             Name = name1,
-            Id = hosts.Max(h => h.Id) + 1,
-            //Id = hosts.Any() ? hosts.Max(h => h.Id) + 1 : 1,
+            Id = hosts.Max(h => h.Id) + 1, 
             Apartments = new List<Apartment>
             {
                 new Apartment { Name = name2, Area = area }
